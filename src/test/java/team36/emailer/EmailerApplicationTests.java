f8d718ce4c087aefbfbaafdb5e55c543809738c8
kang <kang6@ewhain.net>
2024-03-13 01:37:57 +0900

    chore: crate project

---
+++ src/test/java/team36/emailer/EmailerApplicationTests.java
@@ -0,0 +1,13 @@
+package team36.emailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
