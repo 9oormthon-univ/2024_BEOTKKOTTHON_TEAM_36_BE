9cfcfbfa2f71feb9c7584b17f1f375e3d7579edb
kang <kang6@ewhain.net>
2024-03-14 01:03:42 +0900

    feat: create Success msg enum

---
+++ src/main/java/mongkey/maeilmail/common/response/Success.java
@@ -0,0 +1,35 @@
+package mongkey.maeilmail.common.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Success {
+
+    // Default
+    SUCCESS(HttpStatus.OK, "Request successfully processed"),
+
+
+    //200 SUCCESS
+    GET_ONE_POST_SUCCESS(HttpStatus.OK, "하나의 게시글을 성공적으로 조회하였습니다."),
+
+
+
+
+    //201 CREATED SUCCESS
+    CREATE_POST_SUCCESS(HttpStatus.CREATED, "게시글을 성공적으로 등록하였습니다."),
+
+
+    ;
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+}
