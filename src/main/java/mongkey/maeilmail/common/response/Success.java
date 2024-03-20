1839071f5151c6650c65b2161c48f93263a83d73
kang <kang6@ewhain.net>
2024-03-20 23:02:54 +0900

    chore: add success msg

--- src/main/java/mongkey/maeilmail/common/response/Success.java
+++ src/main/java/mongkey/maeilmail/common/response/Success.java
@@ -20,7 +20,7 @@ public enum Success {
     //201 CREATED SUCCESS
     CREATE_POST_SUCCESS(HttpStatus.CREATED, "게시글을 성공적으로 등록하였습니다."),
     CREATE_EMAIL_SUCCESS(HttpStatus.CREATED, "성공적으로 이메일 전문을 생성하였습니다."),
-
+    RECREATE_EMAIL_SUCCESS(HttpStatus.CREATED, "성공적으로 이메일을 재생성하였습니다."),
 
     ;
 
