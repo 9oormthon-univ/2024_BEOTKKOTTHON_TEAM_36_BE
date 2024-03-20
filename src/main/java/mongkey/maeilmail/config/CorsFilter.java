7fa2842959ede2c41dbf52e7d2b0e406d33dee38
kang <kang6@ewhain.net>
2024-03-21 02:07:25 +0900

    chore: edit access control allow origin

--- src/main/java/mongkey/maeilmail/config/CorsFilter.java
+++ src/main/java/mongkey/maeilmail/config/CorsFilter.java
@@ -15,8 +15,8 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5502");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5502");
+        response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
