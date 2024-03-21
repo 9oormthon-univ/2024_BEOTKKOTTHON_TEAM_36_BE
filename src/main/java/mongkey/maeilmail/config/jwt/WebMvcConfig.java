91ccc34d4a8cce85044e8721460a12c19b641702
Shim Hun <shimhun88@naver.com>
2024-03-21 17:43:22 +0900

    chore: 인터셉터 엔드포인트 추가

--- src/main/java/mongkey/maeilmail/config/jwt/WebMvcConfig.java
+++ src/main/java/mongkey/maeilmail/config/jwt/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package mongkey.maeilmail.config.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
     private final BearerAuthInterceptor bearerAuthInterceptor;
 
@@ -14,9 +16,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     public void addInterceptors(InterceptorRegistry registry){
-        System.out.println(">>> 인터셉터 등록");
+        System.out.println(">>> 인터셉터 호출");
 
         //어느 엔드포인트까지 인터셉터를 추가할지 생각해야함
         registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/post");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/post/*");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/template");
+        registry.addInterceptor(bearerAuthInterceptor).addPathPatterns("/api/template/*");
+
+
     }
 }
