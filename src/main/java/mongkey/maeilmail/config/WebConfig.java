8ee8dd8afe513afcede774694c1163ee69a3e353
kang <kang6@ewhain.net>
2024-03-22 00:31:49 +0900

    chore: recreate config for set allowd origin

---
+++ src/main/java/mongkey/maeilmail/config/WebConfig.java
@@ -0,0 +1,18 @@
+package mongkey.maeilmail.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5502")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+        ;
+    }
+
+}
